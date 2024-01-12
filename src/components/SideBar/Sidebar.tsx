b402d3f53b1d0e0c9029e506b70ed56f3a5f44ee
Javier Ricardo Ramirez Alegria <javirramirez@falabella.cl>
2024-01-12 20:11:53 -0300

    cambio color de fondo

--- src/components/SideBar/Sidebar.tsx
+++ src/components/SideBar/Sidebar.tsx
@@ -12,12 +12,12 @@ export default function Sidebar(props: Props) {
       <p>Tap the cards and find the pairs! have fun for hours</p>
       <p>Your score is:</p>
       <div className="row">
-      <p className="alert alert-success mx-1 col-xl-12 col-md-5">
-        Successes: <b>{successes}</b>
-      </p>
-      <p className="alert alert-danger mx-1 col-xl-12 col-md-5">
-        Mistakes: <b>{mistakes}</b>
-      </p>
+        <p className="alert alert-success mx-1 col-xl-12 col-md-5">
+          Right: <b>{successes}</b>
+        </p>
+        <p className="alert alert-danger mx-1 col-xl-12 col-md-5">
+          Wrong: <b>{mistakes}</b>
+        </p>
       </div>
     </>
   );
